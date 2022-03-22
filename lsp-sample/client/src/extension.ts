69611323dd1c54429eebea97e75d3858c0c99e7e
Jordan Eldredge <jordan@jordaneldredge.com>
2022-03-22 15:13:48 -0700

    Hack sample LSP to use Relay's binary

--- lsp-sample/client/src/extension.ts
+++ lsp-sample/client/src/extension.ts
@@ -7,6 +7,7 @@ import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
 
 import {
+	Executable,
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
@@ -17,32 +18,50 @@ let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 	// The server is implemented in node
+	/*
 	const serverModule = context.asAbsolutePath(
 		path.join('server', 'out', 'server.js')
 	);
+	*/
+
+	// const serverPath = "/Users/captbaritone/projects/relay/compiler/target/release/relay-bin";
+	const serverPath = "/Users/captbaritone/lsp.sh";
 	// The debug options for the server
 	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	const debugOptions = { execArgv: [] };
+
+	const args = ["relay.config.json", "--lsp"];
+
+	const newEnv = Object.assign({}, process.env);
+	Object.assign(newEnv, {});
+
+	const run: Executable = {
+		command: serverPath,
+		args,
+		options: { env: newEnv },
+	};
+
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
+		run,
+		debug: run,
 	};
 
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
+		documentSelector: [{ scheme: 'file', language: 'javascript' },
+		{ scheme: 'file', language: 'typescript' },
+		{ scheme: 'file', language: 'typescriptreact' },
+		{ scheme: 'file', language: 'javascriptreact' },
+		],
+
+		// TODO: Config?
+		// synchronize: {
+		// Notify the server about file changes to '.clientrc files contained in the workspace
+		//			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		//	}
 	};
 
 	// Create the language client and start the client.
